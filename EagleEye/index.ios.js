31638e50ed534a92c916fd68b60273530f1c664a
Grant Timmerman <granttimmerman@gmail.com>
2015-11-21 18:35:50 -0600

    Finished the ios Tab Bar

--- EagleEye/index.ios.js
+++ EagleEye/index.ios.js
@@ -16,21 +16,22 @@ var {
 
 var EagleEye = React.createClass({
   render: function() {
+      // <View style={styles.container}>
+      //   <PlaneMap/>
+      //   <Text style={styles.welcome}>
+      //     Welcome to Stuff!
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     To get started, edit index.ios.js
+      //   </Text>
+      //   <Text style={styles.instructions}>
+      //     Press Cmd+R to reload,{'\n'}
+      //     Cmd+D or shake for dev menu
+      //   </Text>
+
+      // </View>
     return (
-      <View style={styles.container}>
-        <PlaneMap/>
-        <Text style={styles.welcome}>
-          Welcome to Stuff!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-        <AppTabBar/>
-      </View>
+      <AppTabBar/>
     );
   }
 });
